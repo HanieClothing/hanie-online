9fdc1534ece7c2fa166910fee27d7922aa1e7f2f
yapyeeqiang <yapyeeqiang@gmail.com>
2025-04-16 15:58:01 +0800

    refactor: center

--- app/(private)/store/components/IncentivesSection.tsx
+++ app/(private)/store/components/IncentivesSection.tsx
@@ -27,25 +27,18 @@ function IncentivesSection() {
     <div className="bg-white">
       <div className="mx-auto max-w-7xl sm:px-2 lg:px-4 pt-24 sm:pt-32">
         <div className="mx-auto max-w-2xl px-4 lg:max-w-none">
-          <div className="max-w-3xl">
-            <h2 className="text-4xl font-bold tracking-tight text-gray-900">
+          <div>
+            <h2 className="text-4xl text-center font-bold tracking-tight text-gray-900">
               We built our business on customer service
             </h2>
-            <p className="mt-4 text-gray-500">
-              At the beginning at least, but then we realized we could make a
-              lot more money if we kinda stopped caring about that. Our new
-              strategy is to write a bunch of things that look really good in
-              the headlines, then clarify in the small print but hope people
-              don't actually read it.
-            </p>
           </div>
           <div className="mt-16 grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
             {incentives.map((incentive) => (
               <div key={incentive.name} className="sm:flex lg:block">
-                <div className="sm:shrink-0">
+                <div className="sm:shrink-0 flex items-center justify-center">
                   <img alt="" src={incentive.imageSrc} className="size-16" />
                 </div>
-                <div className="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6">
+                <div className="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6 text-center">
                   <h3 className="text-sm font-medium text-gray-900">
                     {incentive.name}
                   </h3>
